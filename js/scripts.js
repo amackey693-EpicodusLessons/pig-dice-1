7ba7ccaba549a54905d7fb05d3e1f901a4931bec
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-04-09 10:20:12 -0700

    add function that displays current score to user on html.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 //for Player -->
 function Player () {
   this.playerName = "";
-  this.totalScore = 0;
+  this.totalScore = [];
   this.turnScore = 0;
 };
 
@@ -21,17 +21,32 @@ Player.prototype.rollDice = function() {
   } else if (random !== 1) {
     this.turnScore += random;
   } 
-
-  console.log("turn score", this.turnScore);
-  console.log("Random", random);
+  // console.log("turn score", this.turnScore);
+  // console.log("Random", random);
 };
 
 // "Hold Button" Method --> Adds to the total score
-Player.prototype.sumScore = function(){
-  var total = this.totalScore;
-  var roll = this.turnScore;
-    total += roll;
+Player.prototype.sumScore = function(){  
+  var sum = [];
+  sum.push(this.turnScore);
+
+  sum.reduce(function (accumulator, currentValue, currentIndex, array) {
+    return accumulator + currentValue;
+  });
+  this.totalScore.push(sum);
+  console.log("sum", sum)
 };
+
+//   var hold = this.turnScore + this.totalScore;
+//   if (this.turnScore === 0) {
+//     hold = 0;
+//   } 
+//   if (this.turnScore > 1) {
+  
+//   }
+//   return hold
+//   console.log("hold", hold)
+
   
 // Optional "Game" Construct to keep track of which players turn it is??
 
@@ -48,22 +63,43 @@ Player.prototype.sumScore = function(){
 // .append() | .html()
 // if we set turnscore to 0 var total would = random number rolled + 0, and on next roll it would be random number rolled + previously stored integer in turnscore.
 
+function showPlayer(){
+  // var contact = addressBook.findContact(contactId);
+ 
+
+  // $("#").html(player.totalScore);
+
+}
+
+  // var buttons = $("#buttons");
+  // buttons.empty();
+  // buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
+  // console.log("contactId", contactId)
+
 $(document).ready(function() {
-  var playerOne = new Player(); 
-  $("button#roll").click(function() {
-    var output = playerOne.rollDice();
-  $("button#hold").click(function(){  
-    return playerOne.sumScore();
-  });
-    console.log("output", output);
+  var playerOne = new Player();
+  var playerTwo = new Player(); 
+
+  $("button#roll1").click(function () {
+    playerOne.rollDice();
+    $(".player-name1").html(playerOne.playerName);
+    $(".current-score1").html(playerOne.turnScore);
     
-    
-   
-   
-    
-    $("#output").append(output);
   });
 
+  $("button#roll2").click(function () {
+    playerTwo.rollDice();
+    $(".player-name2").html(playerTwo.playerName);
+    $(".current-score2").html(playerTwo.turnScore);
+    
+  });
+
+  $("button#hold").click(function(){  
+    playerOne.sumScore()
+    $(".game-score1").html(playerOne.totalScore);
+  });
+    
+  // debugger;
   
 });
 
